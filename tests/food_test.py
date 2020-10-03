ebf967e8886b1de3ab5871473ca632494b5ca810
Kate <you@example.com>
2020-10-03 21:45:20 +0300

    making tests

---
+++ tests/food_test.py
@@ -0,0 +1,24 @@
+import unittest
+from models import food
+
+Food=food.Food
+
+class FoodTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the displayed food list
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+
+        self.new_food = Food(1234,'Python Must Be Crazy','2019-10-10')
+        
+    def test_instance(self)
+        self.assertTrue(isinstance(self.new_food,Food))
+
+if __name__ == "__main__":
+    main
+
+         
