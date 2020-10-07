fccb2684296ea8691ec6a988ef341224aa9ed44f
Kate <you@example.com>
2020-10-07 05:12:16 +0300

    email validation

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-
+from flak_mail import Mail
+from flask import email_validator
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 export GIPHY_API_KEY='UAls2uVXRJMSsgznjfckjsOl7bvhs8CD'
 export SECRET_KEY='1234'
 
+
+export MAIL_USERNAME='catherinekadogo001@gmail.com'
+export MAIL_PASSWORD='ybedqdhshpfxkqto'
+
+
 python3 manage.py server
